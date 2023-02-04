19d8e2c3553e8871a1c9d5455ad1f4c976a443c4
harcourtian <suryanshhbtu@gmail.com>
2023-02-05 02:10:02 +0530

    added CORS-Cross_Origin_Resource_Sharing in app.js

--- app.js
+++ app.js
@@ -4,9 +4,10 @@ const newsRouter = require('./api/routes/news');
 const app = express();              // executing express
 const morgan = require('morgan');
 const bodyParser = require('body-parser');
+const res = require('express/lib/response');
 
 app.use(bodyParser.urlencoded({extended:false}));
-app.use(bodyParser.json());
+app.use(bodyParser.json());            
 app.use('/news', newsRouter);
 
 app.use(morgan('dev'));             // it logs all the requests made to server GET/PATCH/POST/DELETE -> GET /news/special 200 7.979 ms - 23
@@ -23,6 +24,16 @@ app.use((error, req, res, next) => {       // ERROR INDORMATION INSIDE HTML
             message : error.message
         }
     })
+});
+
+// Handling CORS Errors -> Cross-Origin-Resourse-Sharing
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST, PATCH, DELETE, GET');
+    }
+    return res.status(200).json({});
 });
 
 module.exports = app;
