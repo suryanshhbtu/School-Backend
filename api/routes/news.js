549a291190191c86b1c68ad92d8c88cd075a3c71
harcourtian <suryanshhbtu@gmail.com>
2023-02-05 00:46:32 +0530

    added morgan, -dev nodemon, body-parser

--- api/routes/news.js
+++ api/routes/news.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const router = express.Router();
+const router = express.Router();     // importing router from express
 
 router.get('/', (req, res, next) => {
     res.status(200).json({
@@ -21,4 +21,17 @@ router.get('/:newsId', (req, res, next) => {
     
 });
 
+router.post('/', (req, res, next)=>{
+    console.log(req.body);
+    const news = {
+        title: req.body.title,
+        msg: req.body.msg,
+        isNew: req.body.isNew,
+        type: req.body.type
+    }
+    res.status(200).json({
+        message: "Handling Post",
+        news: news,
+    });
+});
 module.exports = router;
--- app.js
+++ app.js
@@ -2,12 +2,27 @@
 const express = require('express'); // importing express
 const newsRouter = require('./api/routes/news');
 const app = express();              // executing express
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
 app.use('/news', newsRouter);
 
-app.use((req, res, next) => {       // use is middleware  which accepts arrow function (request, response, next)
-    res.status(200).json({
-        message: 'It Works!'
-    });
+app.use(morgan('dev'));             // it logs all the requests made to server GET/PATCH/POST/DELETE -> GET /news/special 200 7.979 ms - 23
+
+app.use((req, res, next) => {       // ERROR INFORMATION INSIDE HTML
+    error.status = 404;
+    next(error);
 });
+// this  code converts above html error to json error
+app.use((error, req, res, next) => {       // ERROR INDORMATION INSIDE HTML
+    res.status(error.status || 500);
+    res.json({
+        error:{
+            message : error.message
+        }
+    })
+});
+
 module.exports = app;
