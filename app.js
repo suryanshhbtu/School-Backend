0ea8342de240de301fe5840d2ee83a49b7305e1b
harcourtian <suryanshhbtu@gmail.com>
2023-02-05 23:36:34 +0530

    Moved CORS to the top in app.js -> error removed

--- app.js
+++ app.js
@@ -8,6 +8,19 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+
+
+// Handling CORS Errors -> Cross-Origin-Resourse-Sharing
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Headers','Origin,X-Requested-With,Content-Type,Accept,Authorization');
+    if(req.method === 'OPTIONS'){
+        res.header('Access-Control-Allow-Methods','PUT,POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+    }
+    next();
+});
+
 mongoose.set("strictQuery", false);
 mongoose.connect('mongodb+srv://suryanshhbtu:suryanshhbtu@cluster0.t1zpklg.mongodb.net/?retryWrites=true&w=majority', () => {
   console.log("Connected to MongoDB");
@@ -35,16 +48,6 @@ app.use((error, req, res, next) => {       // ERROR INDORMATION INSIDE HTML
             message: error.message
         }
     })
-});
-
-// Handling CORS Errors -> Cross-Origin-Resourse-Sharing
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Headers', 'Origin,X-Requested-With,Content-Type,Accept,Authorization');
-    if (req.method === 'OPTIONS') {
-        res.header('Access-Control-Allow-Methods', 'PUT,POST, PATCH, DELETE, GET');
-    }
-    return res.status(200).json({});
 });
 
 module.exports = app;
